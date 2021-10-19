a91253a10e5d58309e631e8adb800751dc773e1f
R A Mislu <mislura1998@gmail.com>
2021-10-19 23:23:57 +0600

    file submit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCI-GWH5BGFWLhLK5lP3xcBGJyawZgbOhc",
+  authDomain: "react-assign10.firebaseapp.com",
+  projectId: "react-assign10",
+  storageBucket: "react-assign10.appspot.com",
+  messagingSenderId: "936279719122",
+  appId: "1:936279719122:web:2cbc0e72a478228186410b",
+};
+export default firebaseConfig;
